04fefa87f701cb605dc5c4b33dc7618a01c7b493
nasvt-phys117 <63495021+nasvt-phys117@users.noreply.github.com>
2025-02-05 21:00:42 +0200

    Multiple tasks can be stored and displayed

--- src/index.js
+++ src/index.js
@@ -10,27 +10,64 @@ var displayTaskBtn = document.getElementById('displayTaskBtn');
 
 
 let testTask = new ToDoTask(1, toDoTaskTitle, toDoTaskDescription);
-function addTaskTesting(todoTask){
-  localStorage.setItem("taskTitle", todoTask.title);
-  localStorage.setItem("taskDescription", todoTask.description);
+let testTaskArray = [];
+
+function addTaskTesting(todoTask, taskArray){
+
+  let inToDoTaskObj = {id: 1, title: todoTask.title, description: todoTask.description}
+
+
+  taskArray.push(inToDoTaskObj);
+  let jsonArray = JSON.stringify(taskArray);
+  localStorage.setItem("taskArray",jsonArray);
+
+  // localStorage.setItem("taskTitle", todoTask.title);
+  // localStorage.setItem("taskDescription", todoTask.description);
 }
 
 addTaskBtn.addEventListener('click', (event) => {
-  addTaskTesting(testTask);
+  addTaskTesting(testTask,testTaskArray);
   testTask.logTask();
+
+  toDoTaskTitle.value = '';
+  toDoTaskDescription.value = '';
 });
 
 displayTaskBtn.addEventListener('click', (event)=> {
   const taskOutput = document.getElementById('taskOutput');
-  const outToDoTaskTitle = localStorage.getItem('taskTitle');
-  const outTodoTaskDescription = localStorage.getItem('taskDescription');
 
-  while (taskOutput.firstChild) {
-    taskOutput.removeChild(taskOutput.lastChild);
+  let localJsonArray = localStorage.getItem("taskArray");
+
+  if (localJsonArray) {
+    let taskArray = JSON.parse(localJsonArray);
+    console.log(taskArray);
+
+    while (taskOutput.firstChild) {
+      taskOutput.removeChild(taskOutput.lastChild);
+    }
+    taskOutput.appendChild(document.createElement("h2"))
+      .textContent = "Tasks:";
+
+    const outputText = taskOutput.appendChild(document.createElement("p"));
+
+    for (var i = 0; i < taskArray.length; i++)
+    {
+      outputText.innerHTML += "<br> <br>" +"<b>Title: </b>" +taskArray[i].title + "<br> <b>Description</b>: " + taskArray[i].description;
+    }
+
+  } else {
+    console.log("Error: No local data found!");
   }
-  taskOutput.appendChild(document.createElement("h2"))
-  .textContent = "Tasks:";
 
-  const outputText = taskOutput.appendChild(document.createElement("p"));
-  outputText.innerHTML = outToDoTaskTitle;
+  // const outToDoTaskTitle = localStorage.getItem('taskTitle');
+  // const outTodoTaskDescription = localStorage.getItem('taskDescription');
+
+  // while (taskOutput.firstChild) {
+  //   taskOutput.removeChild(taskOutput.lastChild);
+  // }
+  // taskOutput.appendChild(document.createElement("h2"))
+  // .textContent = "Tasks:";
+
+  // const outputText = taskOutput.appendChild(document.createElement("p"));
+  // outputText.innerHTML = outToDoTaskTitle  + "<br> <b>Description</b>: " + outTodoTaskDescription;
 });
